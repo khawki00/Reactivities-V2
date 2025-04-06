f55005a315edff1b19b21c6acd25ee26aba8a6a2
Keesha Hawkins <khawki00@hotmail.com>
2025-04-06 03:39:54 -0400

    End of section 5

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,10 +4,10 @@ import { createRoot } from 'react-dom/client'
 // import '@fontsource/roboto/400.css';
 // import '@fontsource/roboto/500.css';
 // import '@fontsource/roboto/700.css';
-import './index.css'
+import './app/layout/styles.css'
 
 
-import App from './App.tsx'
+import App from './app/layout/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
