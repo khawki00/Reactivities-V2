ecb680dacb480bd09a409e08a34cf723807c31b3
Keesha Hawkins <khawki00@hotmail.com>
2025-04-20 14:07:21 -0400

    End of section seven routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,15 +6,16 @@ import { createRoot } from "react-dom/client";
 // import '@fontsource/roboto/700.css';
 import "./app/layout/styles.css";
 
-import App from "./app/layout/App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router";
+import { router } from "./app/router/Routes.tsx";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
     <ReactQueryDevtools  />
-    <App />
+    <RouterProvider router={router}/>
     </QueryClientProvider>
   
   </StrictMode>
